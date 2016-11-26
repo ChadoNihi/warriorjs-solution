b4c7c87b68768e80ccbf8c60e05199e6bd2551d2
ChadoNihi <chadoishere@gmail.com>
2016-11-27 00:29:08 +0200

    lvl 7

--- efish-beginner/Player.js
+++ efish-beginner/Player.js
@@ -3,17 +3,32 @@ class Player {
     this.prevHP;
     this.maxHP = -1;
     this.enemyCnt = 3;
-    this.enemyWasAhead = false;
+    this.resqCnt = 1;
   }
 
   playTurn(warrior) {
     const empty = warrior.feel().isEmpty(),
+          emptyBack = warrior.feel('backward').isEmpty(),
           currHP = warrior.health();
 
     this.maxHP = Math.max(this.maxHP, currHP);
 
-    if (warrior.feel().isCaptive()) {
-      warrior.rescue();
+    if (this.resqCnt > 0 && emptyBack) {
+      warrior.walk('backward');
+    }
+    else if (warrior.feel('backward').isCaptive()) {
+      warrior.rescue('backward');
+      this.resqCnt -= 1;
+    }
+    else if (this.enemyCnt === 1 && currHP > 15) {
+      if (empty) {
+        warrior.walk();
+      } else {
+        warrior.attack();
+      }
+    }
+    else if (currHP < 8 && currHP < this.prevHP && emptyBack) {
+      warrior.walk('backward');
     }
     else if (empty) {
       if (this.enemyWasAhead) {
