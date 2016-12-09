42c9690859c9f1c2f986186007b93ab75bf5da31
ChadoNihi <chadoishere@gmail.com>
2016-12-09 18:48:12 +0200

    lvl 9

--- efish-beginner/Player.js
+++ efish-beginner/Player.js
@@ -2,42 +2,48 @@ class Player {
   constructor() {
     this.prevHP;
     this.maxHP = -1;
-    this.enemyCnt = 2;
-    this.resqCnt = 1;
+    //this.enemyCnt = 2;
+    this.rescCnt = 1;
     this.left = false;
   }
 
   playTurn(warrior) {
-    const empty = warrior.feel().isEmpty(),
-          emptyBack = warrior.feel('backward').isEmpty(),
-          wall = warrior.feel().isWall(),
-          //wallBack = warrior.feel('backward').isWall(),
-          currHP = warrior.health();
+    const space = warrior.feel(),
+          spaceBack = warrior.feel('backward'),
+          empty = space.isEmpty(),
+          emptyBack = spaceBack.isEmpty(),
+          enemy = space.isEnemy(),
+          wall = space.isWall(),
+          wallBack = spaceBack.isWall(),
+          currHP = warrior.health(),
+          numOfEnemiesToShootAhead = warrior.look().reduce((cnt, sp)=> {
+            if (sp.isEnemy()) return cnt + 1;
+            else return cnt;
+          }, 0);
 
     this.maxHP = Math.max(this.maxHP, currHP);
 
-    if (wall) {
-      warrior.pivot();
-      this.left = !this.left;
+    if (space.isCaptive()) {
+      warrior.rescue();
+      this.rescCnt -= 1;
     }
-    else if (currHP < 8 && currHP < this.prevHP && emptyBack) {
+    else if (this.rescCnt > 0) {
+      warrior.walk();
+    }
+    else if (currHP < 8 && currHP < prevHP && numOfEnemiesToShootAhead > 1) {
       warrior.walk('backward');
     }
-    else if (empty) {
-      if (this.enemyWasAhead) {
-        this.enemyCnt -= 1;
-        this.enemyWasAhead = false;
-      }
-
-      if (currHP < this.maxHP && currHP >= this.prevHP && this.enemyCnt > 0) {
-        warrior.rest();
-      } else {
-        warrior.walk();
-      }
+    else if (enemy) {
+      warrior.attack();
+    }
+    else if (currHP < this.maxHP && numOfEnemiesToShootAhead > 0) {
+      warrior.rest();
+    }
+    else if (numOfEnemiesToShootAhead > 0) {
+      warrior.shoot();
     }
     else {
-      warrior.attack();
-      this.enemyWasAhead = true;
+      warrior.walk();
     }
 
     this.prevHP = currHP;
